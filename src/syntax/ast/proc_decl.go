c18164977e4822783bd6c63e82584ee3f60d5505
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-12-13 11:31:30 +0000

    add more data structures for procedure-declaration

--- src/syntax/ast/proc_decl.go
+++ src/syntax/ast/proc_decl.go
@@ -1,10 +1,42 @@
 package ast
 
+import "github.com/anthonyabeo/obx/src/syntax/token"
+
+type Receiver struct {
+	Mod  token.Token
+	Var  *Ident
+	Type *Ident
+}
+
+type ProcHead struct {
+	Rcv  *Receiver
+	Name *Ident
+	FP   *FormalParams
+}
+
+type ProcBody struct {
+	DeclSeq []Declaration
+	StmtSeq []Statement
+}
+
 type ProcDecl struct {
+	Head *ProcHead
+	Body *ProcBody
 }
 
 func (p *ProcDecl) decl() {}
 
 func (p *ProcDecl) String() string {
 	return ""
+}
+
+type FPSection struct {
+	Mod   token.Token
+	Names []*Ident
+	Type  Expression
+}
+
+type FormalParams struct {
+	Params  []*FPSection
+	RetType Expression
 }
