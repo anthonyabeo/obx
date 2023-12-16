81ef7138267a1c9c709b3858ddac2583f33400ae
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-12-16 13:11:07 +0000

    initial implementation of the global scope

--- src/sema/global.go
+++ src/sema/global.go
@@ -1,1 +1,53 @@
 package sema
+
+import (
+	"github.com/anthonyabeo/obx/src/sema/types"
+)
+
+var Global *Scope
+
+var Typ = []*types.Basic{
+	types.Invalid: types.NewBasicType(types.Invalid, 0, "invalid type"),
+
+	types.Bool:  types.NewBasicType(types.Bool, types.IsBoolean, "bool"),
+	types.Int8:  types.NewBasicType(types.Int8, types.IsInteger, "int8"),
+	types.Int16: types.NewBasicType(types.Int16, types.IsInteger, "int16"),
+	types.Int32: types.NewBasicType(types.Int32, types.IsInteger, "int32"),
+	types.Int64: types.NewBasicType(types.Int64, types.IsInteger, "int64"),
+}
+
+func defPredeclaredTypes() {
+	for _, t := range Typ {
+		Global.Insert(NewTypeName(nil, t.Name(), t))
+	}
+}
+
+// A builtinId is the id of a builtin function.
+type builtinId int
+
+const (
+	// universe scope
+	_Assert builtinId = iota
+)
+
+var predeclaredProcedures = [...]struct {
+	name  string
+	nargs int
+}{
+	_Assert: {"append", 1},
+}
+
+func defPredeclaredProcedures() {
+	for i := range predeclaredProcedures {
+		id := builtinId(i)
+
+		Global.Insert(newBuiltin(id))
+	}
+}
+
+func init() {
+	Global = NewScope(nil, "global")
+
+	defPredeclaredTypes()
+	defPredeclaredProcedures()
+}
