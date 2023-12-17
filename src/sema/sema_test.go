887cf37b934425631dde230f889a5ff576670855
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-12-17 17:48:45 +0000

    beginnings of semantics analysis tests

---
+++ src/sema/sema_test.go
@@ -0,0 +1,50 @@
+package sema
+
+import (
+	"testing"
+
+	"github.com/anthonyabeo/obx/src/syntax/lexer"
+	"github.com/anthonyabeo/obx/src/syntax/parser"
+	"github.com/anthonyabeo/obx/src/syntax/token"
+)
+
+func TestTypeCheckMinimalProgram(t *testing.T) {
+	input := `
+module Main
+	proc fib(n : integer): integer
+		var a, b: integer 
+
+  	begin
+		if (n = 0) or (n = 1) then
+			return n
+		else
+		  a := fib(n - 1)
+		  b := fib(n - 2)
+		  return a + b
+		end
+  	end fib
+
+begin
+	res := fib(21)
+  	assert(res = 10946)
+end Main
+`
+
+	file := token.NewFile("test.obx", len([]byte(input)))
+	lex := &lexer.Lexer{}
+	lex.InitLexer(file, []byte(input))
+
+	p := &parser.Parser{}
+	p.InitParser(lex)
+	ob := p.Oberon()
+
+	sema := &Visitor{}
+	sema.InitSemaVisitor(ob, Global)
+	sema.VisitModule("Main")
+	if len(sema.errors) > 0 {
+		t.Error("found semantic errors")
+		for _, err := range sema.errors {
+			t.Log(err)
+		}
+	}
+}
