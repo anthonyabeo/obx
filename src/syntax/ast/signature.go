eb35f0922786fcd849e47fbdc1c78f0376a6a4a3
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-12-18 13:11:12 +0000

    move Signature to ast package

---
+++ src/syntax/ast/signature.go
@@ -0,0 +1,41 @@
+package ast
+
+import "github.com/anthonyabeo/obx/src/sema/types"
+
+type Signature struct {
+	rcv    *Receiver
+	fp     *FormalParams
+	Params []DesugParam
+}
+
+func NewSignature(rcv *Receiver, params *FormalParams) (sig *Signature) {
+	sig = &Signature{rcv: rcv, fp: params}
+	sig.DeSugarParams()
+	return
+}
+
+func (t *Signature) ReturnType() Expression {
+	return t.fp.RetType
+}
+
+func (t *Signature) NumParams() int {
+	return len(t.Params)
+}
+
+func (t *Signature) DeSugarParams() {
+	for _, FP := range t.fp.Params {
+		for _, name := range FP.Names {
+			t.Params = append(t.Params, DesugParam{Name: name, Type: FP.Type})
+		}
+	}
+}
+
+func (t *Signature) Underlying() types.Type { return t }
+func (t *Signature) String() string         { return "" }
+
+// DesugParam
+// ----------------
+type DesugParam struct {
+	Name *Ident
+	Type Expression
+}
