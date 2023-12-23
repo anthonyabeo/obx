7af368fb9884f3733c2622ba5b427465e6c471f1
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-12-23 16:30:01 +0000

    define array type

--- src/syntax/ast/array.go
+++ src/syntax/ast/array.go
@@ -8,18 +8,18 @@ import (
 // An ArrayType represents an array type AST node.
 type ArrayType struct {
 	Array    *token.Position
-	lenList  *LenList
-	elemType Expression
+	LenList  *LenList
+	ElemType Expression
 	EType    types.Type
 }
 
 func NewArray(pos *token.Position, lenList *LenList, elem Expression) *ArrayType {
-	return &ArrayType{Array: pos, lenList: lenList, elemType: elem}
+	return &ArrayType{Array: pos, LenList: lenList, ElemType: elem}
 }
 
 func (a *ArrayType) expr()                {}
-func (a *ArrayType) Pos() *token.Position { panic("not implemented") }
-func (a *ArrayType) End() *token.Position { panic("not implemented") }
+func (a *ArrayType) Pos() *token.Position { return a.Array }
+func (a *ArrayType) End() *token.Position { return a.ElemType.End() }
 func (a *ArrayType) String() string       { panic("not implemented") }
 func (a *ArrayType) Accept(vst Visitor)   { vst.VisitArrayType(a) }
 func (a *ArrayType) Type() types.Type     { return a.EType }
