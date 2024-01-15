ada88784cd93757870b5895545c0fc335db90790
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2024-01-15 11:31:59 +0000

    beginnings of the implementation of the ILOC specification

---
+++ src/codegen/iloc.go
@@ -0,0 +1,44 @@
+package codegen
+
+type Opcode int
+
+const (
+	Invalid Opcode = iota
+
+	Add
+	Sub
+	Mult
+	Div
+	LShift
+	RShift
+)
+
+type InstrKind int
+
+const (
+	Error InstrKind = iota
+
+	Labeled
+	CtrlFlow
+	Normal
+)
+
+type Instruction interface {
+	Opcode() Opcode
+	Kind() InstrKind
+}
+
+type OperandKind int
+
+const (
+	Err OperandKind = iota
+
+	Register
+	Number
+	Label
+)
+
+type Operand interface {
+	Name() string
+	Kind() OperandKind
+}
