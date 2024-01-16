1d21bf4fdf45515ea6fb0248fd013a86ad13973b
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2024-01-16 12:56:57 +0000

    add VisitModule method to translate.Visitor

--- src/translate/visitor.go
+++ src/translate/visitor.go
@@ -17,200 +17,211 @@ type Visitor struct {
 func NewVisitor(ast *ast.Oberon, env *sema.Scope) *Visitor {
 	return &Visitor{ast: ast, env: env}
 }
+func (v *Visitor) VisitModule(name string) {
+	module := v.ast.Program[name]
 
-func (i *Visitor) VisitIdentifier(ident *ast.Ident) {
+	for _, decl := range module.DeclSeq {
+		decl.Accept(v)
+	}
+
+	for _, stmt := range module.StmtSeq {
+		stmt.Accept(v)
+	}
+}
+
+func (v *Visitor) VisitIdentifier(ident *ast.Ident) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitBinaryExpr(expr *ast.BinaryExpr) {
+func (v *Visitor) VisitBinaryExpr(expr *ast.BinaryExpr) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitDesignator(designator *ast.Designator) {
+func (v *Visitor) VisitDesignator(designator *ast.Designator) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitFuncCall(call *ast.FuncCall) {
+func (v *Visitor) VisitFuncCall(call *ast.FuncCall) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitUnaryExpr(expr *ast.UnaryExpr) {
+func (v *Visitor) VisitUnaryExpr(expr *ast.UnaryExpr) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitQualifiedIdent(ident *ast.QualifiedIdent) {
+func (v *Visitor) VisitQualifiedIdent(ident *ast.QualifiedIdent) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitSet(set *ast.Set) {
+func (v *Visitor) VisitSet(set *ast.Set) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitBasicLit(lit *ast.BasicLit) {
+func (v *Visitor) VisitBasicLit(lit *ast.BasicLit) {
 	switch lit.Kind {
 	case token.INT:
 
 	}
 }
 
-func (i *Visitor) VisitIfStmt(stmt *ast.IfStmt) {
+func (v *Visitor) VisitIfStmt(stmt *ast.IfStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitAssignStmt(stmt *ast.AssignStmt) {
+func (v *Visitor) VisitAssignStmt(stmt *ast.AssignStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitReturnStmt(stmt *ast.ReturnStmt) {
+func (v *Visitor) VisitReturnStmt(stmt *ast.ReturnStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitProcCall(call *ast.ProcCall) {
+func (v *Visitor) VisitProcCall(call *ast.ProcCall) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitRepeatStmt(stmt *ast.RepeatStmt) {
+func (v *Visitor) VisitRepeatStmt(stmt *ast.RepeatStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitWhileStmt(stmt *ast.WhileStmt) {
+func (v *Visitor) VisitWhileStmt(stmt *ast.WhileStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitLoopStmt(stmt *ast.LoopStmt) {
+func (v *Visitor) VisitLoopStmt(stmt *ast.LoopStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitCaseStmt(stmt *ast.CaseStmt) {
+func (v *Visitor) VisitCaseStmt(stmt *ast.CaseStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitForStmt(stmt *ast.ForStmt) {
+func (v *Visitor) VisitForStmt(stmt *ast.ForStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitExitStmt(stmt *ast.ExitStmt) {
+func (v *Visitor) VisitExitStmt(stmt *ast.ExitStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitWithStmt(stmt *ast.WithStmt) {
+func (v *Visitor) VisitWithStmt(stmt *ast.WithStmt) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitImport(imp *ast.Import) {
+func (v *Visitor) VisitImport(imp *ast.Import) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitProcDecl(decl *ast.ProcDecl) {
+func (v *Visitor) VisitProcDecl(decl *ast.ProcDecl) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitVarDecl(decl *ast.VarDecl) {
+func (v *Visitor) VisitVarDecl(decl *ast.VarDecl) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitConstDecl(decl *ast.ConstDecl) {
+func (v *Visitor) VisitConstDecl(decl *ast.ConstDecl) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitTypeDecl(decl *ast.TypeDecl) {
+func (v *Visitor) VisitTypeDecl(decl *ast.TypeDecl) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitBasicType(basicType *ast.BasicType) {
+func (v *Visitor) VisitBasicType(basicType *ast.BasicType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitArrayType(arrayType *ast.ArrayType) {
+func (v *Visitor) VisitArrayType(arrayType *ast.ArrayType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitPointerType(pointerType *ast.PointerType) {
+func (v *Visitor) VisitPointerType(pointerType *ast.PointerType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitProcType(procType *ast.ProcType) {
+func (v *Visitor) VisitProcType(procType *ast.ProcType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitRecordType(recordType *ast.RecordType) {
+func (v *Visitor) VisitRecordType(recordType *ast.RecordType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitEnumType(enumType *ast.EnumType) {
+func (v *Visitor) VisitEnumType(enumType *ast.EnumType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitReceiver(receiver *ast.Receiver) {
+func (v *Visitor) VisitReceiver(receiver *ast.Receiver) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitProcHead(head *ast.ProcHead) {
+func (v *Visitor) VisitProcHead(head *ast.ProcHead) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitProcBody(body *ast.ProcBody) {
+func (v *Visitor) VisitProcBody(body *ast.ProcBody) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitFPSection(section *ast.FPSection) {
+func (v *Visitor) VisitFPSection(section *ast.FPSection) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitFormalParams(params *ast.FormalParams) {
+func (v *Visitor) VisitFormalParams(params *ast.FormalParams) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitDotOp(op *ast.DotOp) {
+func (v *Visitor) VisitDotOp(op *ast.DotOp) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitIndexOp(op *ast.IndexOp) {
+func (v *Visitor) VisitIndexOp(op *ast.IndexOp) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitTypeGuard(guard *ast.TypeGuard) {
+func (v *Visitor) VisitTypeGuard(guard *ast.TypeGuard) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i *Visitor) VisitPointerDeref(deref *ast.PointerDeref) {
+func (v *Visitor) VisitPointerDeref(deref *ast.PointerDeref) {
 	//TODO implement me
 	panic("implement me")
 }
