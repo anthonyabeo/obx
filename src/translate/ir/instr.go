b9bd01d5fe27a39a20086b295728171b032c1242
Anthony A. Abeo <anthonyabeo@gmail.com>
2024-01-25 06:14:03 +0000

    begin transition away from ILOC

--- src/translate/ir/instr.go
+++ src/translate/ir/instr.go
@@ -5,99 +5,32 @@ import (
 	"strings"
 )
 
-type InstrKind int
-
-const (
-	Error InstrKind = iota
-
-	Labeled
-	CtrlFlow
-	Normal
-)
-
 type Instruction interface {
 	Opcode() Opcode
-	Kind() InstrKind
+	IsTerm() bool
+	IsBinaryOp() bool
 	String() string
 }
-type LabeledInstr struct {
-	label string
-	instr []Instruction
-}
-
-func CreateLabeledInstr(label string, kind InstrKind) *LabeledInstr {
-	return &LabeledInstr{label: label}
-}
-
-func (l LabeledInstr) Opcode() Opcode {
-	if len(l.instr) > 0 {
-		return l.instr[0].Opcode()
-	}
-
-	return Invalid
-}
-
-func (l LabeledInstr) Kind() InstrKind { return Labeled }
-
-func (l LabeledInstr) String() string { panic("not implemented") }
-
-// NormalInstr performs computation or data movement.
-// They consist of an opcode, a list of comma-separated
-// source operands, and a list of comma-separated result operands.
-// ---------------------------------------------------------------
-type NormalInstr struct {
-	op     Opcode
-	srcOps []Operand
-	dstOps []Operand
-}
-
-func CreateNormalInstr(op Opcode, src, dst []Operand) *NormalInstr {
-	return &NormalInstr{op: op, srcOps: src, dstOps: dst}
-}
-func (n NormalInstr) Opcode() Opcode { return n.op }
-
-func (n NormalInstr) Kind() InstrKind { return Normal }
-
-func (n NormalInstr) String() string {
-	var src, dst []string
-	for _, op := range n.srcOps {
-		src = append(src, op.String())
-	}
-
-	for _, op := range n.dstOps {
-		dst = append(dst, op.String())
-	}
-
-	return fmt.Sprintf("%s %s => %s", n.op, strings.Join(src, ", "), strings.Join(dst, ", "))
-}
-
-// CtlFlowInstr used to change the flow of control in the program.
-// ----------------------------------------------------------------
-type CtlFlowInstr struct {
-	op Opcode
-}
-
-func CreateCtlFlowInstr(op Opcode) *CtlFlowInstr { return &CtlFlowInstr{op: op} }
-
-func (c CtlFlowInstr) Opcode() Opcode { return c.op }
-
-func (c CtlFlowInstr) Kind() InstrKind { return CtrlFlow }
-
-func (c CtlFlowInstr) String() string { panic("not implemented") }
 
 // CallInstr ...
 // --------------------
 type CallInstr struct {
-	Proc string
-	Args []Operand
+	op     Opcode
+	Proc   string
+	Args   []Operand
+	Result Operand
+}
+
+func (c CallInstr) IsTerm() bool {
+	return termop_begin < c.op && c.op < termop_end
+}
+
+func (c CallInstr) IsBinaryOp() bool {
+	return binop_end < c.op && c.op < binop_end
 }
 
 func (c CallInstr) Opcode() Opcode {
-	return call
-}
-
-func (c CallInstr) Kind() InstrKind {
-	return Normal
+	return c.op
 }
 
 func (c CallInstr) String() string {
@@ -106,7 +39,7 @@ func (c CallInstr) String() string {
 		args = append(args, op.String())
 	}
 
-	return fmt.Sprintf("call %s(%s)", c.Proc, strings.Join(args, ", "))
+	return fmt.Sprintf("%s = call %s(%s)", c.Result, c.Proc, strings.Join(args, ", "))
 }
 
 // CmpInstr ...
@@ -117,10 +50,127 @@ type CmpInstr struct {
 	Result Operand
 }
 
+func (c CmpInstr) IsTerm() bool {
+	return termop_begin < c.Cond && c.Cond < termop_end
+}
+
+func (c CmpInstr) IsBinaryOp() bool {
+	return binop_begin < c.Cond && c.Cond < binop_end
+}
+
 func (c CmpInstr) Opcode() Opcode { return c.Cond }
 
-func (c CmpInstr) Kind() InstrKind { return Normal }
-
 func (c CmpInstr) String() string {
-	return fmt.Sprintf("cmp %s %s %s => %s", c.Cond, c.X, c.Y, c.Result)
+	return fmt.Sprintf("%s = cmp %s %s, %s", c.Result, c.Cond, c.X, c.Y)
+}
+
+func CreateCmp(cond Opcode, left, right Operand, name string) *CmpInstr {
+	if name == "" {
+		// TODO Make next temp more global?
+		// name = tmp
+
+	}
+
+	return &CmpInstr{Cond: cond, X: left, Y: right, Result: Register{Name: name, OpKind: KRegister}}
+}
+
+// BinaryOp ...
+// --------------------
+type BinaryOp struct {
+	op     Opcode
+	Left   Operand
+	Right  Operand
+	Result Operand
+}
+
+func (b BinaryOp) Opcode() Opcode {
+	return b.op
+}
+
+func (b BinaryOp) IsTerm() bool {
+	return termop_begin < b.op && b.op < termop_end
+}
+
+func (b BinaryOp) IsBinaryOp() bool {
+	return binop_begin < b.op && b.op < binop_end
+}
+
+func (b BinaryOp) String() string {
+	return fmt.Sprintf("%s = %s %s, %s", b.Result, b.op, b.Left, b.Right)
+}
+
+func CreateAdd(left, right Operand, name string) *BinaryOp {
+	if name == "" {
+		// TODO Make next temp more global?
+		// name = tmp
+
+	}
+
+	return &BinaryOp{op: Add, Left: left, Right: right, Result: Register{Name: name, OpKind: KRegister}}
+}
+
+// LoadInst ...
+// ----------------
+type LoadInst struct {
+	op  Opcode
+	src Operand
+	res Operand
+}
+
+func (l LoadInst) Opcode() Opcode {
+	return l.op
+}
+
+func (l LoadInst) IsTerm() bool {
+	return termop_begin < l.op && l.op < termop_end
+}
+
+func (l LoadInst) IsBinaryOp() bool {
+	return binop_begin < l.op && l.op < binop_end
+}
+
+func (l LoadInst) String() string {
+	return fmt.Sprintf("%s = load %s", l.res, l.src)
+}
+
+func CreateLoad(src Operand, name string) *LoadInst {
+	if name == "" {
+		// TODO Make next temp more global?
+		// name = tmp
+	}
+
+	return &LoadInst{op: Load, src: src, res: Register{Name: name, OpKind: KRegister}}
+}
+
+// StoreInst ...
+// -------------------
+type StoreInst struct {
+	op    Opcode
+	value Operand
+	dst   Operand
+}
+
+func (s StoreInst) Opcode() Opcode {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s StoreInst) IsTerm() bool {
+	return termop_begin < s.op && s.op < termop_end
+}
+
+func (s StoreInst) IsBinaryOp() bool {
+	return binop_begin < s.op && s.op < binop_end
+}
+
+func (s StoreInst) String() string {
+	return fmt.Sprintf("store %s, %s", s.value, s.dst)
+}
+
+func CreateStore(value, dst Operand) *StoreInst {
+	return &StoreInst{
+		op:    Store,
+		value: value,
+		dst:   dst,
+	}
 }
