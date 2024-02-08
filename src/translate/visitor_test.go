f947fe81b77877246097fd20562cd277ae155cb6
Anthony A. Abeo <anthonyabeo@gmail.com>
2024-02-08 06:04:46 +0000

    add WhileStmt codegen test for multi-elsif branches

--- src/translate/visitor_test.go
+++ src/translate/visitor_test.go
@@ -84,6 +84,90 @@ end Main
 	fmt.Println(Main)
 }
 
+func TestIRCodegenWhileLoopWithSingleElseIfBranch(t *testing.T) {
+	input := `
+module Main
+	var m, n, gcd: integer
+
+begin
+	m := 24
+    n := 48
+	gcd := 0
+	
+	while m > n do
+		m := m + n
+    elsif n > m do
+		n := n + m
+	end
+
+    assert(gcd = 55)
+end Main
+`
+
+	file := token.NewFile("test.obx", len([]byte(input)))
+	lex := &lexer.Lexer{}
+	lex.InitLexer(file, []byte(input))
+
+	p := &parser.Parser{}
+	p.InitParser(lex)
+	ob := p.Oberon()
+
+	scp := sema.NewScope(sema.Global, "Main")
+
+	sema_vst := &sema.Visitor{}
+	sema_vst.InitSemaVisitor(ob, scp)
+	sema_vst.VisitModule("Main")
+
+	cgen := NewVisitor(ob, scp)
+	module := cgen.VisitModule("Main")
+
+	Main := module.GetFunction("main")
+	fmt.Println(Main)
+}
+
+func TestIRCodegenWhileLoopWithTwoElseIfBranch(t *testing.T) {
+	input := `
+module Main
+	var m, n, gcd: integer
+
+begin
+	m := 24
+    n := 48
+	gcd := 0
+	
+	while m > n do
+		m := m + n
+    elsif n > m do
+		n := n + m
+	elsif m # n do
+		n := n + m
+	end
+
+    assert(gcd = 55)
+end Main
+`
+
+	file := token.NewFile("test.obx", len([]byte(input)))
+	lex := &lexer.Lexer{}
+	lex.InitLexer(file, []byte(input))
+
+	p := &parser.Parser{}
+	p.InitParser(lex)
+	ob := p.Oberon()
+
+	scp := sema.NewScope(sema.Global, "Main")
+
+	sema_vst := &sema.Visitor{}
+	sema_vst.InitSemaVisitor(ob, scp)
+	sema_vst.VisitModule("Main")
+
+	cgen := NewVisitor(ob, scp)
+	module := cgen.VisitModule("Main")
+
+	Main := module.GetFunction("main")
+	fmt.Println(Main)
+}
+
 func TestIRCodegenRepeatStmt(t *testing.T) {
 	input := `
 module Main
