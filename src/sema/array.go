0d4c987580faa2962a6e6f7bb9948a441b6d96e7
Anthony Abeo <anthonyabeo@gmail.com>
2024-02-21 11:59:18 +0000

    compute Width() of closed arrays

--- src/sema/array.go
+++ src/sema/array.go
@@ -1,24 +1,44 @@
 package sema
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/anthonyabeo/obx/src/sema/types"
 	"github.com/anthonyabeo/obx/src/syntax/ast"
 )
 
 // An Array represents an array type.
 type Array struct {
-	len  ast.Expression
-	elem types.Type
+	LenList *ast.LenList
+	ElemTy  types.Type
 }
 
-func NewArray(elem types.Type, len ast.Expression) *Array { return &Array{len: len, elem: elem} }
-
-func (a *Array) Len() ast.Expression { return a.len }
-
-func (a *Array) Elem() types.Type { return a.elem }
+func NewArray(elem types.Type, len *ast.LenList) *Array {
+	return &Array{LenList: len, ElemTy: elem}
+}
 
 func (a *Array) Underlying() types.Type { return a }
 
-func (a *Array) String() string { return "" }
+func (a *Array) String() string {
+	var ll []string
+	for _, l := range a.LenList.List {
+		ll = append(ll, l.String())
+	}
 
-func (a *Array) Width() int { panic("not implemented") }
+	return fmt.Sprintf("[%s]%s", strings.Join(ll, ", "), a.ElemTy)
+}
+
+func (a *Array) Width() int {
+	w := a.ElemTy.Width()
+
+	for i := len(a.LenList.List) - 1; i <= 0; i-- {
+		w *= a.LenList.List[i].Type().Width()
+	}
+
+	return w
+}
+
+func (a *Array) IsOpen() bool {
+	return a.LenList == nil
+}
