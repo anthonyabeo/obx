9c7c0c0e80c13cbc9304557f5be0cfc819e60bbc
Anthony A. Abeo <anthonyabeo@gmail.com>
2024-03-07 00:18:04 +0000

    flesh out ast.Oberon

--- src/syntax/ast/oberon.go
+++ src/syntax/ast/oberon.go
@@ -1,9 +1,55 @@
 package ast
 
+import "fmt"
+
 type Oberon struct {
-	Program map[string]*Module
+	program map[string]Unit
 }
 
 func NewOberon() *Oberon {
-	return &Oberon{Program: make(map[string]*Module)}
+	return &Oberon{program: make(map[string]Unit)}
+}
+
+func (obx *Oberon) Module(name string) (*Module, error) {
+	m, ok := obx.program[name]
+	if !ok {
+		return nil, fmt.Errorf("no module named %s found", name)
+	}
+
+	mod, ok := m.(*Module)
+	if !ok {
+		return nil, fmt.Errorf("%s is not a module", m.Name())
+	}
+
+	return mod, nil
+}
+
+func (obx *Oberon) Definition(name string) (*Definition, error) {
+	m, ok := obx.program[name]
+	if !ok {
+		return nil, fmt.Errorf("no definition named %s found", name)
+	}
+
+	def, ok := m.(*Definition)
+	if !ok {
+		return nil, fmt.Errorf("%s is not a definition", m.Name())
+	}
+
+	return def, nil
+}
+
+func (obx *Oberon) AddUnit(name string, unit Unit) error {
+	if u, ok := obx.program[name]; ok {
+		if u.Kind() == unit.Kind() {
+			return fmt.Errorf("unit with name %s already defined", name)
+		}
+	}
+
+	obx.program[name] = unit
+
+	return nil
+}
+
+func (obx *Oberon) Program() map[string]Unit {
+	return obx.program
 }
