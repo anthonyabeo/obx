f1375be2ee65e958d970f2f086e6649581e77417
Anthony A. Abeo <anthonyabeo@gmail.com>
2024-03-08 16:22:23 +0000

    Model Oberon programs a directed graph of modules

--- src/syntax/ast/oberon.go
+++ src/syntax/ast/oberon.go
@@ -2,6 +2,10 @@ package ast
 
 import "fmt"
 
+// Oberon is the top-level representation of an Oberon+ program
+// It is modelled as directed-acyclic graph where the nodes in the graph
+// are compilation units (Module and Definition) and the edges between
+// them represent their import dependency.
 type Oberon struct {
 	program map[string]Unit
 }
@@ -13,12 +17,12 @@ func NewOberon() *Oberon {
 func (obx *Oberon) Module(name string) (*Module, error) {
 	m, ok := obx.program[name]
 	if !ok {
-		return nil, fmt.Errorf("no module named %s found", name)
+		return nil, fmt.Errorf("no module named '%s' found", name)
 	}
 
 	mod, ok := m.(*Module)
 	if !ok {
-		return nil, fmt.Errorf("%s is not a module", m.Name())
+		return nil, fmt.Errorf("'%s' is not a module", m.Name())
 	}
 
 	return mod, nil
@@ -27,12 +31,12 @@ func (obx *Oberon) Module(name string) (*Module, error) {
 func (obx *Oberon) Definition(name string) (*Definition, error) {
 	m, ok := obx.program[name]
 	if !ok {
-		return nil, fmt.Errorf("no definition named %s found", name)
+		return nil, fmt.Errorf("no definition named '%s' found", name)
 	}
 
 	def, ok := m.(*Definition)
 	if !ok {
-		return nil, fmt.Errorf("%s is not a definition", m.Name())
+		return nil, fmt.Errorf("'%s' is not a definition", m.Name())
 	}
 
 	return def, nil
@@ -41,7 +45,7 @@ func (obx *Oberon) Definition(name string) (*Definition, error) {
 func (obx *Oberon) AddUnit(name string, unit Unit) error {
 	if u, ok := obx.program[name]; ok {
 		if u.Kind() == unit.Kind() {
-			return fmt.Errorf("unit with name %s already defined", name)
+			return fmt.Errorf("unit with name '%s' already defined", name)
 		}
 	}
 
--- src/syntax/ast/unit.go
+++ src/syntax/ast/unit.go
@@ -15,6 +15,7 @@ type Unit interface {
 	Name() string
 	Kind() UnitKind
 	Accept(Visitor)
+	Incident() []string
 }
 
 // Module
@@ -27,12 +28,18 @@ type Module struct {
 	ImportList []*Import
 	DeclSeq    []Declaration
 	StmtSeq    []Statement
+
+	incidence []string
 }
 
 func (m *Module) Accept(vst Visitor)   { vst.VisitModule(m) }
 func (m *Module) Name() string         { return m.BeginName.Name }
 func (m *Module) Kind() UnitKind       { return MOD }
 func (m *Module) Pos() *token.Position { return m.Mod }
+func (m *Module) Incident() []string   { return m.incidence }
+func (m *Module) AddAdjacent(name string) {
+	m.incidence = append(m.incidence, name)
+}
 
 type MetaSection struct {
 	Mode    token.Token
@@ -48,8 +55,14 @@ type Definition struct {
 	EndName    *Ident
 	ImportList []*Import
 	DeclSeq    []Declaration
+
+	incidence []string
 }
 
 func (def *Definition) Accept(vst Visitor) { vst.VisitDefinition(def) }
 func (def *Definition) Name() string       { return def.BeginName.Name }
 func (def *Definition) Kind() UnitKind     { return DEF }
+func (def *Definition) Incident() []string { return def.incidence }
+func (def *Definition) AddAdjacent(name string) {
+	def.incidence = append(def.incidence, name)
+}
