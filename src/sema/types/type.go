4e28e3c7961723230c6382383a931893bc7a0c05
Anthony Abeo <anthonyabeo@gmail.com>
2024-03-11 21:16:53 +0000

    Introduce the Generic type

--- src/sema/types/type.go
+++ src/sema/types/type.go
@@ -7,3 +7,30 @@ type Type interface {
 	Underlying() Type
 	Width() int // number of bytes (8-bit) this type requires
 }
+
+func NewGenericType(name string, tyConst Type) *Generic {
+	return &Generic{
+		Name:    name,
+		TyConst: tyConst,
+	}
+}
+
+type Generic struct {
+	Name    string
+	TyConst Type
+}
+
+func (g Generic) String() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (g Generic) Underlying() Type {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (g Generic) Width() int {
+	//TODO implement me
+	panic("implement me")
+}
