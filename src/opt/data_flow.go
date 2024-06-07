6f3f3f65b4bfb0864489efd629948b8f5ac544e5
Anthony Abeo <anthonyabeo@gmail.com>
2024-06-07 11:21:42 +0000

    introduce alternate implementation of reaching definition using iterative dataflow algorithm

--- src/opt/data_flow.go
+++ src/opt/data_flow.go
@@ -8,30 +8,20 @@ import (
 // ----------------------------------------------
 type WorkList struct {
 	queue []string
-	set   map[string]bool
 }
 
 func NewWorkList(elems []string) *WorkList {
-	set := map[string]bool{}
-	for _, elem := range elems {
-		set[elem] = true
-	}
-
-	return &WorkList{queue: elems, set: set}
+	return &WorkList{queue: elems}
 }
 
 func (w *WorkList) Add(elem string) {
-	if _, exist := w.set[elem]; !exist {
-		w.set[elem] = true
-		w.queue = append(w.queue, elem)
-	}
+	w.queue = append(w.queue, elem)
 }
 
 func (w *WorkList) Dequeue() string {
 	elem := w.queue[0]
 	w.queue = w.queue[1:]
 
-	delete(w.set, elem)
 	return elem
 }
 
@@ -39,7 +29,12 @@ func (w *WorkList) Empty() bool {
 	return len(w.queue) == 0
 }
 
-// IterativeDataFlow
+// IterativeDataFlow implements an iterative data-flow algorithm for the
+// reaching definitions problem.
+//
+// It used a lattice of 8-bit-wide-vectors. It takes in as input, 'Init' the
+// initial value of the 'entry' node and F: BV^8 -> BV^8, a set of flow functions
+// as explained in Muchnick 8.4
 // ----------------------------------------------
 func IterativeDataFlow(cfg *ir.ControlFlowGraph, F map[string]func(ir.BitVector) ir.BitVector, Init ir.BitVector) map[string]ir.BitVector {
 	rpo := cfg.ReversePostOrder()
@@ -73,4 +68,37 @@ func IterativeDataFlow(cfg *ir.ControlFlowGraph, F map[string]func(ir.BitVector)
 	}
 
 	return DFIn
+}
+
+func IterativeDataflowDragonBook(cfg *ir.ControlFlowGraph, GEN, KILL map[string]ir.BitVector) (map[string]ir.BitVector, map[string]ir.BitVector) {
+	rpo := cfg.ReversePostOrder()
+	rpo = rpo[1:]
+
+	workList := NewWorkList(rpo)
+
+	IN := map[string]ir.BitVector{}
+	OUT := map[string]ir.BitVector{}
+	for name := range cfg.Nodes {
+		OUT[name] = ir.BitVector(0)
+	}
+
+	for !workList.Empty() {
+		B := workList.Dequeue()
+
+		IN[B] = ir.BitVector(0)
+		for _, name := range cfg.Pred[B] {
+			IN[B] |= OUT[name]
+		}
+
+		prevOut := OUT[B]
+		OUT[B] = GEN[B] | (IN[B] & ^KILL[B])
+
+		if prevOut != OUT[B] {
+			for _, succ := range cfg.Succ[B] {
+				workList.Add(succ)
+			}
+		}
+	}
+
+	return IN, OUT
 }
