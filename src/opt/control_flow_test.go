71ad72e19dab2a6e4bce06de8e17490d4e860bf5
Anthony A. Abeo <anthonyabeo@gmail.com>
2024-06-11 04:25:06 +0000

    add tests for computing dominance frontiers of CFG nodes

--- src/opt/control_flow_test.go
+++ src/opt/control_flow_test.go
@@ -149,10 +149,17 @@ func TestComputeDominance(t *testing.T) {
 		{"exit", []string{"entry", "B1", "B4", "exit"}},
 	}
 
-	dom := Dominator(cfg, entry)
+	dom := Dominance(cfg)
 	for _, tt := range tests {
 		if len(tt.blocks) != len(dom[tt.name]) {
 			t.Errorf("expected a dom set of size %d, got %d", len(tt.blocks), len(dom[tt.name]))
+		}
+
+		for i := 0; i < len(tt.blocks); i++ {
+			set, exists := dom[tt.name]
+			if !exists || !set.Contains(tt.blocks[i]) {
+				t.Errorf("expected DOM['%s'] to contain '%s'", tt.name, tt.blocks[i])
+			}
 		}
 	}
 }
@@ -201,7 +208,7 @@ func TestComputeDominance2(t *testing.T) {
 		{"exit", []string{"entry", "B3", "exit"}},
 	}
 
-	dom := Dominator(cfg, entry)
+	dom := Dominance(cfg)
 	for _, tt := range tests {
 		if len(tt.blks) != len(dom[tt.name]) {
 			t.Errorf("expected a dom set of size %d, got %d", len(tt.blks), len(dom[tt.name]))
@@ -214,6 +221,85 @@ func TestComputeDominance2(t *testing.T) {
 		}
 	}
 
+}
+
+func TestComputeDominance3(t *testing.T) {
+	b0 := ir.NewBasicBlock("B0")
+	b1 := ir.NewBasicBlock("B1")
+	b2 := ir.NewBasicBlock("B2")
+	b3 := ir.NewBasicBlock("B3")
+	b4 := ir.NewBasicBlock("B4")
+	b5 := ir.NewBasicBlock("B5")
+	b6 := ir.NewBasicBlock("B6")
+	b7 := ir.NewBasicBlock("B7")
+	b8 := ir.NewBasicBlock("B8")
+
+	cfg := &ir.ControlFlowGraph{
+		Entry: b0,
+		Exit:  b4,
+		Nodes: map[string]*ir.BasicBlock{
+			"B0": b0,
+			"B1": b1,
+			"B2": b2,
+			"B3": b3,
+			"B4": b4,
+			"B5": b5,
+			"B6": b6,
+			"B7": b7,
+			"B8": b8,
+		},
+		Succ: map[string][]string{
+			"B0": {"B1"},
+			"B1": {"B2", "B5"},
+			"B2": {"B3"},
+			"B3": {"B4", "B1"},
+			"B4": {},
+			"B5": {"B6", "B8"},
+			"B6": {"B7"},
+			"B7": {"B3"},
+			"B8": {"B7"},
+		},
+		Pred: map[string][]string{
+			"B0": {},
+			"B1": {"B0", "B3"},
+			"B2": {"B1"},
+			"B3": {"B2", "B7"},
+			"B4": {"B3"},
+			"B5": {"B1"},
+			"B6": {"B5"},
+			"B7": {"B6", "B8"},
+			"B8": {"B5"},
+		},
+	}
+
+	tests := []struct {
+		name   string
+		blocks []string
+	}{
+		{"B0", []string{"B0"}},
+		{"B1", []string{"B0", "B1"}},
+		{"B2", []string{"B0", "B1", "B2"}},
+		{"B3", []string{"B0", "B1", "B3"}},
+		{"B4", []string{"B0", "B1", "B3", "B4"}},
+		{"B5", []string{"B0", "B1", "B5"}},
+		{"B6", []string{"B0", "B1", "B5", "B6"}},
+		{"B7", []string{"B0", "B1", "B5", "B7"}},
+		{"B8", []string{"B0", "B1", "B5", "B8"}},
+	}
+
+	Dom := Dominance(cfg)
+	for _, tt := range tests {
+		if len(tt.blocks) != len(Dom[tt.name]) {
+			t.Errorf("expected a dom set of size %d, got %d", len(tt.blocks), len(Dom[tt.name]))
+		}
+
+		for i := 0; i < len(tt.blocks); i++ {
+			set, exists := Dom[tt.name]
+			if !exists || !set.Contains(tt.blocks[i]) {
+				t.Errorf("expected DOM['%s'] to contain '%s'", tt.name, tt.blocks[i])
+			}
+		}
+	}
 }
 
 func TestComputeImmediateDominance(t *testing.T) {
@@ -249,8 +335,8 @@ func TestComputeImmediateDominance(t *testing.T) {
 		},
 	}
 
-	dom := Dominator(cfg, entry)
-	iDom := ImmDominator(cfg, entry, dom)
+	dom := Dominance(cfg)
+	iDom := ImmDominator(cfg, dom)
 
 	tests := []struct {
 		name string
@@ -382,4 +468,84 @@ func TestComputeNaturalLoop2(t *testing.T) {
 			t.Errorf("'%s' should not be part of the natural loop of B3->B1", tt)
 		}
 	}
+}
+
+func TestDominanceFrontier(t *testing.T) {
+	b0 := ir.NewBasicBlock("B0")
+	b1 := ir.NewBasicBlock("B1")
+	b2 := ir.NewBasicBlock("B2")
+	b3 := ir.NewBasicBlock("B3")
+	b4 := ir.NewBasicBlock("B4")
+	b5 := ir.NewBasicBlock("B5")
+	b6 := ir.NewBasicBlock("B6")
+	b7 := ir.NewBasicBlock("B7")
+	b8 := ir.NewBasicBlock("B8")
+
+	cfg := &ir.ControlFlowGraph{
+		Entry: b0,
+		Exit:  b4,
+		Nodes: map[string]*ir.BasicBlock{
+			"B0": b0,
+			"B1": b1,
+			"B2": b2,
+			"B3": b3,
+			"B4": b4,
+			"B5": b5,
+			"B6": b6,
+			"B7": b7,
+			"B8": b8,
+		},
+		Succ: map[string][]string{
+			"B0": {"B1"},
+			"B1": {"B2", "B5"},
+			"B2": {"B3"},
+			"B3": {"B4", "B1"},
+			"B4": {},
+			"B5": {"B6", "B8"},
+			"B6": {"B7"},
+			"B7": {"B3"},
+			"B8": {"B7"},
+		},
+		Pred: map[string][]string{
+			"B0": {},
+			"B1": {"B0", "B3"},
+			"B2": {"B1"},
+			"B3": {"B2", "B7"},
+			"B4": {"B3"},
+			"B5": {"B1"},
+			"B6": {"B5"},
+			"B7": {"B6", "B8"},
+			"B8": {"B5"},
+		},
+	}
+
+	tests := []struct {
+		name   string
+		blocks []string
+	}{
+		{"B0", []string{}},
+		{"B1", []string{"B1"}},
+		{"B2", []string{"B3"}},
+		{"B3", []string{"B1"}},
+		{"B4", []string{}},
+		{"B5", []string{"B3"}},
+		{"B6", []string{"B7"}},
+		{"B7", []string{"B3"}},
+		{"B8", []string{"B7"}},
+	}
+
+	DF := DominanceFrontier(cfg)
+	for _, tt := range tests {
+		if len(tt.blocks) != len(DF[tt.name]) {
+			t.Errorf("expected a dom set of size %d, got %d", len(tt.blocks), len(DF[tt.name]))
+		}
+
+		for i := 0; i < len(tt.blocks); i++ {
+			set, exist := DF[tt.name]
+			if !exist || !set.Contains(tt.blocks[i]) {
+				t.Errorf("expected DF of '%s' to contain '%s'", tt.name, tt.blocks[i])
+			}
+		}
+	}
+
 }
