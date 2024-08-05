a1c1f5afd4aff9e6737ec9f0dd7d9b3eba06dfb9
Anthony Abeo <anthonyabeo@gmail.com>
2024-08-05 14:54:17 +0000

    implement beginnings of algebraic simplification

--- src/opt/scalar.go
+++ src/opt/scalar.go
@@ -41,5 +41,66 @@ func performBinaryOp(op tacil.Opcode, left, right *tacil.ConstantInt) tacil.Expr
 	return tacil.NewConstantInt(left.Ty, result, result < 0)
 }
 
-// AlgebraicSimReAssoc
+// AlgebraicSimplifyReAssociate
 // --------------------------------------------------------------
+func AlgebraicSimplifyReAssociate(assign *tacil.Assign) {
+	switch e := assign.Value.(type) {
+	case *tacil.BinaryOp:
+		_, leftBool := e.X.Type().(tacil.Int1)
+		_, rightBool := e.Y.Type().(tacil.Int1)
+
+		if leftBool && rightBool {
+			assign.Dst = simplifyBool(e)
+		}
+
+	}
+}
+
+func simplifyBool(e *tacil.BinaryOp) (r tacil.Expr) {
+	left, leftBoolConst := e.X.(*tacil.ConstantBool)
+	right, rightBoolConst := e.Y.(*tacil.ConstantBool)
+	if leftBoolConst && rightBoolConst {
+		switch e.Op {
+		case tacil.Or:
+			r = tacil.NewConstBool(left.Value | right.Value)
+		case tacil.And:
+			r = tacil.NewConstBool(left.Value & right.Value)
+		}
+	}
+
+	if leftBoolConst && !rightBoolConst {
+		switch e.Op {
+		case tacil.Or:
+			if left.Value == 1 {
+				r = tacil.NewConstBool(1)
+			} else {
+				r = right
+			}
+		case tacil.And:
+			if left.Value == 1 {
+				r = right
+			} else {
+				r = tacil.NewConstBool(0)
+			}
+		}
+	}
+
+	if !leftBoolConst && rightBoolConst {
+		switch e.Op {
+		case tacil.Or:
+			if right.Value == 1 {
+				r = tacil.NewConstBool(1)
+			} else {
+				r = left
+			}
+		case tacil.And:
+			if right.Value == 1 {
+				r = left
+			} else {
+				r = tacil.NewConstBool(0)
+			}
+		}
+	}
+
+	return
+}
