52d5ed6145a6fb3d9cef3c73900a54447943642c
Anthony Abeo <anthonyabeo@gmail.com>
2025-10-13 10:21:50 +0000

    add OutputDOT function for emitting DOT files for asm Functions

--- src/ir/asm/spec.go
+++ src/ir/asm/spec.go
@@ -1,23 +1,72 @@
 package asm
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/anthonyabeo/obx/modgraph"
+)
+
 type Module struct {
 	Name    string
 	Funcs   []*Function
-	Globals map[string]*Global
+	Globals map[string]*Symbol
 }
 
 type Function struct {
 	Name     string
+	Params   []*Symbol
 	Result   Type
 	Exported bool
+	IsLeaf   bool
 	Blocks   []*Block
 	Entry    *Block
 	Exit     *Block
 
-	CalleeRegsUed map[string]bool // caller-saved registers used in this function
-	Locals        map[string]LocalInfo
+	CalleeRegsUed []string // caller-saved registers used in this function
+	Locals        map[string]Symbol
 	Spills        map[string]SpillInfo
 	HasCalls      bool // whether this function makes calls
+}
+
+func (fn *Function) OutputDOT() {
+	var sb strings.Builder
+	sb.WriteString("digraph CFG {\n")
+	for _, block := range fn.Blocks {
+		//block := fn.Blocks[key]
+		label := fmt.Sprintf("%s:\\l", block.Label)
+		for _, instr := range block.Instr {
+			label += instr.String() + "\\l"
+		}
+		sb.WriteString(fmt.Sprintf("  %q [shape=box,label=\"%s\"];\n", block.Label, label))
+		for _, succ := range block.Succ {
+			sb.WriteString(fmt.Sprintf("  %q -> %q;\n", block.Label, succ.Label))
+		}
+	}
+	sb.WriteString("}\n")
+
+	dot := sb.String()
+
+	Root, err := modgraph.FindProjectRoot()
+	if err != nil {
+		panic(err)
+	}
+
+	dotFile := fmt.Sprintf("%s/out/%s.asm.dot", Root, fn.Name)
+	pngFile := fmt.Sprintf("%s/out/%s.asm.png", Root, fn.Name)
+
+	if err = os.WriteFile(dotFile, []byte(dot), 0644); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command("dot", "-Tpng", dotFile, "-o", pngFile)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Generated %s\n", pngFile)
+	}
 }
 
 // DFSOrder returns the blocks in depth-first order starting from entry.
